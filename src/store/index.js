06d2700e8baedc181ffc0dc152547ceabe46fb26
KellyReisLee <kristinaalvares28@gmail.com>
2024-03-07 14:47:34 +0000

    Adding and setting redux toolkit

--- src/store/index.js
+++ src/store/index.js
@@ -1,40 +1,34 @@
-import { createStore } from 'redux';
+
+import { createSlice, configureStore } from '@reduxjs/toolkit'
 
 const inicialData = { counter: 0, showCounter: true }
 
-const counterReducer = (state = inicialData, action) => {
-  if (action.type === 'increment') {
-    return {
-      counter: state.counter + 1,
-      showCounter: state.showCounter
-    }
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState: inicialData,
+  // Here you can create methods.
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.value
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter
+    },
   }
-
-  if (action.type === 'increase') {
-    return {
-      counter: state.counter + action.value,
-      showCounter: state.showCounter
-    }
-  }
-
-  if (action.type === 'decrement') {
-    return {
-      counter: state.counter - 1,
-      showCounter: state.showCounter
-    }
-  }
-  if (action.type === 'toggle') {
-    return {
-      showCounter: !state.showCounter,
-      counter: state.counter
-    }
-  }
-
-  return state
-}
+})
 
 
 // recebe a reducer function
-const store = createStore(counterReducer);
+const store = configureStore({
+  reducer: {
+    counter: counterSlice.reducer
+  }
+});
 
 export default store;
